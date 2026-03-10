f537b818a6c7d88e5e46b38f58c76f240d53ef10
Boris <borisusik@yandex.ru>
2026-03-10 15:37:06 +0300

    Рефакторинг

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -12,7 +12,7 @@ AppAsset::register($this);
 
 $this->registerCsrfMetaTags();
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
-$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width,initial-scale=1']);
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
@@ -21,8 +21,9 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
+    <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <link rel="stylesheet" href="/css/style.css">
+    <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
